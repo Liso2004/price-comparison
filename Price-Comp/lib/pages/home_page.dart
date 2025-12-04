95ebcfc7aa6489c661c28358e0383955f4c06498
serjunior66 <kabongoserjunior@gmail.com>
2025-12-04 01:41:48 -0800

    update-home

--- Price-Comp/lib/pages/home_page.dart
+++ Price-Comp/lib/pages/home_page.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
           children: [
             Row(
   children: [
- Expanded(
+Expanded(
   child: TextField(
     controller: _searchCtrl,
     style: const TextStyle(color: Colors.grey),
